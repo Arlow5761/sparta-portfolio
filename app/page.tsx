cfaac57a28ad538fff36bc7bc62ddf5fe98abe97
Arlow5761 <arlow5761@gmail.com>
2024-08-03 23:29:52 +0700

    feat/content

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,19 @@ export default function Home() {
   return (
     <main>
       <div>
-        <div>
+        <div className="text-5xl">
           Profile
         </div>
-        <div>
-          <div>
+        <div className="flex flex-row">
+          <Image
+            src="/pasfoto.jpg"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ height:"5em", width:"auto"}}
+            alt="pasfoto"
+          />
+          <div className="flex flex-col">
             <div>
               Nama : Arlow Emmanuel Hergara
             </div>
@@ -21,37 +29,102 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div>
-        <div>
+      <div className="flex flex-col">
+        <div className="text-5xl">
           Hobbies & Abilities
         </div>
-        <div>
-          <div>
-            <div>
+        <div className="flex flex-row">
+          <div className="flex flex-col">
+            <div className="text-2xl">
               Hobbies
             </div>
-          </div>
-          <div>
             <div>
+              <div>
+                Playing games
+              </div>
+              <div>
+                Making games
+              </div>
+              <div>
+                Sleeping
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="text-2xl">
               Abilites
+            </div>
+            <div>
+              <div>
+                Living
+              </div>
+              <div>
+                Breathing
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div>
-        <div>
+        <div className="text-5xl">
           Projects
         </div>
-        <div>
-
+        <div className="flex flex-col">
+          <div className="flex flex-row">
+            <Image
+              src="/holosimp.jpg"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ height:"5em", width:"7em"}}
+              alt="holosimp"
+            />
+            <div className="flex flex-col">
+              <div>
+                Hololive: Idol Starlight Parade
+              </div>
+              <div>
+                A hololive rhythm fan game
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row">
+            <Image
+              src="/rogadoesntlikecheating.png"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ height:"5em", width:"7em"}}
+              alt="roga doesnt like cheating"
+            />
+            <div className="flex flex-col">
+              <div>
+                Roga Doesn't Like Cheating!
+              </div>
+              <div>
+                An infinite runner game
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div>
-        <div>
+        <div className="text-5xl">
           Skills
         </div>
         <div>
-          
+          <div>
+            Unity Game Development
+          </div>
+          <div>
+            C/C++ Programming
+          </div>
+          <div>
+            Python Programming
+          </div>
+          <div>
+            C# Programming
+          </div>
         </div>
       </div>
     </main>
