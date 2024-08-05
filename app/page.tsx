870819d59c0029a07acd0a2a990254d40f243e82
Arlow5761 <arlow5761@gmail.com>
2024-08-06 06:06:33 +0700

    feat/modularcomponents

--- app/page.tsx
+++ app/page.tsx
@@ -1,132 +1,176 @@
 import Image from "next/image";
 
+function Projects() {
+  const projects = [
+    {
+      thumb : "/holosimp.jpg",
+      title : "Hololive: Idol Starlight Parade",
+      desc : "A hololive rhythm fan game",
+      link : ""
+    },
+    {
+      thumb : "/rogadoesntlikecheating.png",
+      title : "Roga Doesn't Like Cheating!",
+      desc : "An infinite runner game",
+      link : ""
+    }
+  ];
+  
+  const renderedProjects = projects.map((project) => {
+    return (
+      <div key={project.title} className="flex flex-row">
+        <Image
+          src={project.thumb}
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ height:"5em", width:"7em"}}
+          alt={project.title}
+        />
+        <div className="flex flex-col">
+          <div>
+            {project.title}
+          </div>
+          <div>
+            {project.desc}
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+  return renderedProjects;
+}
+
+function Hobbies() {
+  const hobbies = [
+    "Playing games",
+    "Making games",
+    "Sleeping"
+  ];
+
+  const renderedHobbies = hobbies.map((hobby) => {
+    return (
+      <div key={hobby}>
+        {hobby}
+      </div>
+    );
+  });
+
+  return renderedHobbies;
+}
+
+function Abilities() {
+  const abilities = [
+    "Living",
+    "Breathing"
+  ];
+
+  const renderedAbilities = abilities.map((ability) => {
+    return (
+      <div key={ability}>
+        {ability}
+      </div>
+    );
+  });
+
+  return renderedAbilities;
+}
+
+function Skills() {
+  const skills = [
+    "Unity Game Development",
+    "C/C++ Programming",
+    "Python Programming",
+    "C# Programming"
+  ];
+
+  const renderedSkills = skills.map((skill) => {
+    return (
+      <div key={skill}>
+        {skill}
+      </div>
+    );
+  });
+
+  return renderedSkills;
+}
+
 export default function Home() {
   return (
-    <main>
-      <div>
-        <div className="text-5xl">
-          Profile
-        </div>
-        <div className="flex flex-row">
-          <Image
-            src="/pasfoto.jpg"
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ height:"5em", width:"auto"}}
-            alt="pasfoto"
-          />
-          <div className="flex flex-col">
-            <div>
-              Nama : Arlow Emmanuel Hergara
+    <div>
+      <div className="block content-center justify-center text-center bg-gray-950 text-white text-9xl h-48 border-b-8 border-b-black dark:border-b-gray-800">
+            <div className="block align-middle content-center">
+              Hello World!
             </div>
-            <div>
-              Jurusan : Teknik Informatika
-            </div>
-            <div>
-              "Manusia sederhana"
-            </div>
-          </div>
-        </div>
       </div>
-      <div className="flex flex-col">
-        <div className="text-5xl">
-          Hobbies & Abilities
-        </div>
-        <div className="flex flex-row">
-          <div className="flex flex-col">
-            <div className="text-2xl">
-              Hobbies
-            </div>
-            <div>
-              <div>
-                Playing games
-              </div>
-              <div>
-                Making games
-              </div>
-              <div>
-                Sleeping
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <div className="text-2xl">
-              Abilites
-            </div>
-            <div>
-              <div>
-                Living
-              </div>
-              <div>
-                Breathing
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="text-5xl">
-          Projects
-        </div>
-        <div className="flex flex-col">
-          <div className="flex flex-row">
-            <Image
-              src="/holosimp.jpg"
-              width={0}
-              height={0}
-              sizes="100vw"
-              style={{ height:"5em", width:"7em"}}
-              alt="holosimp"
-            />
-            <div className="flex flex-col">
-              <div>
-                Hololive: Idol Starlight Parade
-              </div>
-              <div>
-                A hololive rhythm fan game
-              </div>
-            </div>
+      <main className="m-16">
+        <div className="block m-8">
+          <div className="text-7xl text-center">
+            About Me
           </div>
           <div className="flex flex-row">
             <Image
-              src="/rogadoesntlikecheating.png"
+              src="/pasfoto.jpg"
               width={0}
               height={0}
               sizes="100vw"
-              style={{ height:"5em", width:"7em"}}
-              alt="roga doesnt like cheating"
+              style={{ height:"7rem", width:"auto", marginRight:"1rem"}}
+              alt="pasfoto"
             />
-            <div className="flex flex-col">
+            <div className="flex flex-col text-3xl">
               <div>
-                Roga Doesn't Like Cheating!
+                Nama : Arlow Emmanuel Hergara
               </div>
               <div>
-                An infinite runner game
+                Jurusan : Teknik Informatika
+              </div>
+              <div>
+                &quot;Manusia sederhana&quot;
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div>
-        <div className="text-5xl">
-          Skills
+        <div className="block m-8">
+          <div className="text-7xl text-center">
+            Hobbies & Abilities
+          </div>
+          <div className="flex flex-row">
+            <div className="flex flex-col flex-grow">
+              <div className="text-5xl text-center">
+                Hobbies
+              </div>
+              <div>
+                <Hobbies/>
+              </div>
+            </div>
+            <div className="flex flex-col flex-grow">
+              <div className="text-5xl text-center">
+                Abilites
+              </div>
+              <div>
+                <Abilities/>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <div>
-            Unity Game Development
+        <div className="block m-8">
+          <div className="text-7xl text-center">
+            Projects
           </div>
-          <div>
-            C/C++ Programming
-          </div>
-          <div>
-            Python Programming
-          </div>
-          <div>
-            C# Programming
+          <div className="flex flex-col">
+            <Projects/>
           </div>
         </div>
-      </div>
-    </main>
+        <div className="block m-8">
+          <div className="text-7xl text-center">
+            Skills
+          </div>
+          <div>
+            <Skills/>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
