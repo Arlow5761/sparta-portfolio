a49dd196c81a6fc7a2ff1f3cbf1f38f37a6a5e0f
Arlow5761 <arlow5761@gmail.com>
2024-08-09 10:26:36 +0700

    feat: add navigation links

--- app/navigationlinks.tsx
+++ app/navigationlinks.tsx
@@ -1,9 +1,54 @@
 'use client';
 
-export function NavigationLinks() {
+import Link from "next/link";
+import Image from "next/image";
+
+class LinkType {
+    name: string = "";
+    address: string = "";
+}
+
+function NavigationLink(link : LinkType) {
     return (
-        <div>
-            
+        <div className="flex flex-row text-2xl text-center items-center h-20 max-w-80 p-2 landscape:text-left">
+            <Link href={link.address} className="flex flex-row items-center" >
+                {link.name}
+            </Link>
+        </div>
+    );
+}
+
+export function NavigationLinks() {
+    const links : LinkType[] = [
+        {
+            name: "Profile",
+            address: "/"
+        },
+        {
+            name: "Hobbies",
+            address: "/hobbies"
+        },
+        {
+            name: "Skills",
+            address: "/skills"
+        },
+        {
+            name: "Projects",
+            address: "/projects"
+        },
+        {
+            name: "Achievements",
+            address: "/achievements"
+        }
+    ];
+
+    const linkItems = links.map((link) => {
+        return NavigationLink(link);
+    });
+
+    return (
+        <div className="flex justify-center bg-gray-900 w-full max-w-full h-20 landscape:flex-col landscape:w-64 landscape:h-full">
+            {linkItems}
         </div>
     );
 }
