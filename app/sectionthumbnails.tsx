6bf1bbf5dfa0d54c719357a3ca4604da95e63e79
Arlow5761 <arlow5761@gmail.com>
2024-08-10 05:56:00 +0700

    feat: add section thumbnails

--- app/sectionthumbnails.tsx
+++ app/sectionthumbnails.tsx
@@ -1,9 +1,24 @@
 'use client';
 
+import {links} from "./navigationlinks"
+import { usePathname } from "next/navigation";
+
 export function SectionThumbnails() {
+    const pathname = usePathname();
+    let icon = "error";
+
+    for (const link of links) {
+        if (pathname === link.address) {
+            icon = link.icon;
+            break;
+        }
+    }
+
     return (
-        <div>
-            
+        <div className="flex flex-row overflow-clip h-full w-full justify-center items-center gap-[6vw] landscape:flex-col landscape:gap-[10vh]">
+            <div className="material-symbols-outlined text-center content-center text-[20vh] min-h-[20vh] min-w-[20vh] landscape:min-h-[20vw] landscape:min-w-[20vw] landscape:text-[20vw]">
+                {icon}
+            </div>
         </div>
     );
 }
