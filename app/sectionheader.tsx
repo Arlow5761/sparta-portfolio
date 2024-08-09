744c42f86cdfd4cfe3e9ff015f8d3edeac396c27
Arlow5761 <arlow5761@gmail.com>
2024-08-09 11:25:16 +0700

    fix: change wrong section header layout

--- app/sectionheader.tsx
+++ app/sectionheader.tsx
@@ -3,7 +3,7 @@ import {SectionThumbnails} from "./sectionthumbnails"
 
 export default function SectionHeader() {
     return (
-        <div className="bg-blue-950 block h-2/5 landscape:w-2/5 landscape:h-full">
+        <div className="bg-blue-950 block h-2/5 landscape:w-2/5 landscape:h-full landscape:flex">
             <NavigationLinks/>
             <SectionThumbnails/>
         </div>
