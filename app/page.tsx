a50771f24479c27dc3daf2878737935733589249
Arlow5761 <arlow5761@gmail.com>
2024-08-10 18:36:27 +0700

    feat: add profile page as landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,176 +1,41 @@
 import Image from "next/image";
-
-function Projects() {
-  const projects = [
-    {
-      thumb : "/holosimp.jpg",
-      title : "Hololive: Idol Starlight Parade",
-      desc : "A hololive rhythm fan game",
-      link : ""
-    },
-    {
-      thumb : "/rogadoesntlikecheating.png",
-      title : "Roga Doesn't Like Cheating!",
-      desc : "An infinite runner game",
-      link : ""
-    }
-  ];
-  
-  const renderedProjects = projects.map((project) => {
-    return (
-      <div key={project.title} className="flex flex-row">
-        <Image
-          src={project.thumb}
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ height:"5em", width:"7em"}}
-          alt={project.title}
-        />
-        <div className="flex flex-col">
-          <div>
-            {project.title}
-          </div>
-          <div>
-            {project.desc}
-          </div>
-        </div>
-      </div>
-    );
-  });
-
-  return renderedProjects;
-}
-
-function Hobbies() {
-  const hobbies = [
-    "Playing games",
-    "Making games",
-    "Sleeping"
-  ];
-
-  const renderedHobbies = hobbies.map((hobby) => {
-    return (
-      <div key={hobby}>
-        {hobby}
-      </div>
-    );
-  });
-
-  return renderedHobbies;
-}
-
-function Abilities() {
-  const abilities = [
-    "Living",
-    "Breathing"
-  ];
-
-  const renderedAbilities = abilities.map((ability) => {
-    return (
-      <div key={ability}>
-        {ability}
-      </div>
-    );
-  });
-
-  return renderedAbilities;
-}
-
-function Skills() {
-  const skills = [
-    "Unity Game Development",
-    "C/C++ Programming",
-    "Python Programming",
-    "C# Programming"
-  ];
-
-  const renderedSkills = skills.map((skill) => {
-    return (
-      <div key={skill}>
-        {skill}
-      </div>
-    );
-  });
-
-  return renderedSkills;
-}
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div className="block content-center justify-center text-center bg-gray-950 text-white text-9xl h-48 border-b-8 border-b-black dark:border-b-gray-800">
-            <div className="block align-middle content-center">
-              Hello World!
-            </div>
-      </div>
-      <main className="m-16">
-        <div className="block m-8">
-          <div className="text-7xl text-center">
-            About Me
-          </div>
-          <div className="flex flex-row">
-            <Image
-              src="/pasfoto.jpg"
-              width={0}
-              height={0}
-              sizes="100vw"
-              style={{ height:"7rem", width:"auto", marginRight:"1rem"}}
-              alt="pasfoto"
-            />
-            <div className="flex flex-col text-3xl">
-              <div>
-                Nama : Arlow Emmanuel Hergara
-              </div>
-              <div>
-                Jurusan : Teknik Informatika
-              </div>
-              <div>
-                &quot;Manusia sederhana&quot;
-              </div>
-            </div>
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="bg-gray-900 rounded-xl flex flex-col h-[32rem] w-[40rem] p-8 gap-4">
+        <div className="w-full text-center text-7xl mb-4">
+          Arlow (5761)
+        </div>
+        <div className="flex gap-4">
+          <Image
+            src="/ppsparta.png"
+            width={0}
+            height={0}
+            alt="foto"
+            sizes="100vw"
+            className="min-h-48 min-w-48 h-48 w-48 object-cover"
+          />
+          <div className="h-full text-xl text-center content-center">
+            My name is Arlow Emmanuel Hergara and people usually call me Arlow. I am a student at Institut Teknologi Bandung majoring in Informatics.
           </div>
         </div>
-        <div className="block m-8">
-          <div className="text-7xl text-center">
-            Hobbies & Abilities
-          </div>
-          <div className="flex flex-row">
-            <div className="flex flex-col flex-grow">
-              <div className="text-5xl text-center">
-                Hobbies
-              </div>
-              <div>
-                <Hobbies/>
-              </div>
-            </div>
-            <div className="flex flex-col flex-grow">
-              <div className="text-5xl text-center">
-                Abilites
-              </div>
-              <div>
-                <Abilities/>
-              </div>
-            </div>
-          </div>
+        <div className="w-full text-center text-3xl mt-4 mb-1">
+          Contact Me
         </div>
-        <div className="block m-8">
-          <div className="text-7xl text-center">
-            Projects
-          </div>
-          <div className="flex flex-col">
-            <Projects/>
-          </div>
-        </div>
-        <div className="block m-8">
-          <div className="text-7xl text-center">
-            Skills
-          </div>
+        <div className="flex flex-wrap gap-4 justify-center">
           <div>
-            <Skills/>
+            arlow5761@gmail.com (e-mail)
           </div>
+          <Link href="https://github.com/Arlow5761">
+            https://github.com/Arlow5761 (github)
+          </Link>
+          <Link href="https://arlow5761.itch.io/">
+            https://arlow5761.itch.io/ (itch.io)
+          </Link>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
